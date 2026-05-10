851470d4c0c07ce730d190df115f28f964fb3adb
LOSKIw <loski7@163.com>
2026-05-10 12:45:59 +0800

    update

---
+++ custom_scripts/eval_custom/eval_suctom.sh
@@ -0,0 +1,4 @@
+python3 custom_scripts/eval_custom/evaluate_custom.py \
+  --videos_path /path/to/merged_0_9 \
+  --dimension subject_consistency \
+  --seed_start 0 --seed_end 9
---
+++ custom_scripts/eval_custom/merge.sh
@@ -0,0 +1,5 @@
+python3 custom_scripts/eval_custom/merge_infer_groups.py \
+  --src_a /path/to/group_0_4 \
+  --src_b /path/to/group_5_9 \
+  --out /path/to/merged_0_9 \
+  --method symlink
